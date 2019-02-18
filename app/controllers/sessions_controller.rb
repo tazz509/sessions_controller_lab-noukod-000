7cd2b165795d7dc97bd7e47f69ca7c35a9aca4af
carly <carly@noukod.com>
2019-02-18 16:20:38 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+    #
+  end
+
+  def create
+    # binding.pry
+    name = params[:name]
+    if (!name.nil? && name.empty?) or name.nil?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+       #if params[:name].nil?
+  end
+
+  def destroy
+    session.delete(:name) unless session[:name].nil?
+    redirect_to '/'
+  end
+end
